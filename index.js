c00a950616473233175d25e504cf7734258e59c4
Stavros Singoudis <ssingoudis@gmail.com>
2022-12-27 16:20:15 +0100

    Update 27.12
    contact Form with sending Mails added

---
+++ index.js
@@ -0,0 +1,6 @@
+var contactName = document.getElementById('name').value
+var contactMailAdress = document.getElementById('mail').value
+var contactMessage = document.getElementById('message').value
+
+
+
